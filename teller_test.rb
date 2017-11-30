63ef78672d66c8e75d1476539c2933ce48946095
Unknown <eoinoconn@gmail.com>
2017-11-30 20:43:48 +0000

    17

--- teller_test.rb
+++ teller_test.rb
@@ -1,3 +1,4 @@
+require_relative 'teller.rb'
 require 'test/unit'
 
 class TellerTest < Test::Unit::TestCase
@@ -5,19 +6,25 @@ class TellerTest < Test::Unit::TestCase
   # Called before every test method runs. Can be used
   # to set up fixture information.
   def setup
-    # Do nothing
+    @teller = Teller.new("Sam", 5)
   end
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def teardown
-    # Do nothing
+  def fake_transactions
+    10.times do
+      @teller.processed_transaction
+    end
   end
 
-  # Fake test
-  def test_fail
+  def test_to_s
+    fake_transactions
+    string = @teller.to_s
+    correct_string = "Teller Sam completed 10 transactions in 50\n"
+    assert_equal(correct_string, string, "Error in Teller: to_s method")
+  end
 
-    fail('Not implemented')
+  def test_time_to_process
+    fake_transactions
+    time_to_process = @teller.time_to_process
+    assert_equal(50, time_to_process, "Error with Teller: time_to_process method")
   end
 end
