e5d5044c7048fe74744bc20d419e4b84932ce9ff
Unknown <eoinoconn@gmail.com>
2017-11-30 18:10:45 +0000

    5

--- bank.rb
+++ bank.rb
@@ -1,2 +1,23 @@
+require_relative 'account.rb'
+
 class Bank
+  def initialize
+    @accounts = {}
+  end
+
+  def add_account(id)
+    @accounts[id] = Account.new(id, 0)
+  end
+
+  def deposit(id, amount)
+    @accounts[id].deposit(amount)
+  end
+
+  def withdraw(id, amount)
+    @accounts[id].withdraw(amount)
+  end
+
+  def to_s
+    @accounts.each &:to_s
+  end
 end
