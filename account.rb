96061bd2d97e76fff26098da7ca67d7299b61f98
Unknown <eoinoconn@gmail.com>
2017-11-30 18:05:51 +0000

    4

--- account.rb
+++ account.rb
@@ -1,2 +1,18 @@
 class Account
+  def initialize(id, balance)
+    @id = id
+    @balance = balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+  def to_s
+    "Account #{@id} has balance €#{@balance}"
+  end
 end
